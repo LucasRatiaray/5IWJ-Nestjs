ae4c60102394643e1ba6d6c23b5eb385cf1f2fb5
Lucas Ratiaray <dev@lucasratiaray.fr>
2026-08-31 06:04:31 +0200

    feat(users): scaffold users module with creation dto

---
+++ src/users/dtos/create-user.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsString, MinLength, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(12)
+  @MaxLength(72)
+  password: string;
+}
