68b8b4fed7b00d91db732d1729d9599101b7de4c
Lucas Ratiaray <dev@lucasratiaray.fr>
2026-08-31 06:38:06 +0200

    feat(users): implement basic user crud operations

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private repository: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.repository.create({ email, password });
+    return this.repository.save(user);
+  }
+
+  findAll() {
+    return this.repository.find();
+  }
+
+  findOneById(id: string) {
+    return this.repository.findOneBy({ id });
+  }
+
+  async update(id: string, attr: Partial<User>) {
+    const user = await this.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    Object.assign(user, attr);
+    return this.repository.save(user);
+  }
+
+  async remove(id: string) {
+    const user = await this.findOneById(id);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    return this.repository.remove(user);
+  }
+}
